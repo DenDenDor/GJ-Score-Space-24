3a1bdfb3821d897afd3123ccc22e3c95ae4f7a51
JustLink <linki_mail@mail.ru>
2023-01-21 16:48:45 +0400

    Basic waves

---
+++ GJ Score Space 24/Assets/Scripts/Waves/Wave.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Wave
+{
+    private readonly WavePart[] _parts;
+
+    public Wave(WavePart[] parts)
+    {
+        _parts = parts;
+    }
+    
+    public Enemy SpawnAt(Vector3 position)
+    {
+        var index = Random.Range(0, _parts.Length);
+        var part = _parts[index];
+
+        return part.SpawnAt(position);
+    }
+}
---
+++ GJ Score Space 24/Assets/Scripts/Waves/WavePart.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class WavePart
+{
+    public bool IsEmpty => Count == 0;
+    public int Count { get; private set; }
+    
+    private readonly Enemy _prefab;
+
+    public WavePart(Enemy prefab, int count)
+    {
+        _prefab = prefab;
+
+        Count = count;
+    }
+
+    public Enemy SpawnAt(Vector3 position)
+    {
+        if (Count <= 0)
+            throw new System.InvalidOperationException("Wave part is empty");
+
+        Count--;
+
+        return Object.Instantiate(_prefab, position, Quaternion.identity);
+    }
+}
