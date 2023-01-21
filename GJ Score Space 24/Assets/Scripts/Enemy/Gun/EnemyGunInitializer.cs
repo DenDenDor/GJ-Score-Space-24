ab595bfa7a93eb1916a13a6cc8e6f6c9b784a13f
DenDenDor <dendor333@gmail.com>
2023-01-21 14:07:45 +0300

    Creare a player prefab.

--- GJ Score Space 24/Assets/Scripts/Enemy/Gun/EnemyGunInitializer.cs
+++ GJ Score Space 24/Assets/Scripts/Enemy/Gun/EnemyGunInitializer.cs
@@ -4,9 +4,10 @@ public class EnemyGunInitializer : MonoBehaviour
 {
     [SerializeField] private Gun _gun;
     [SerializeField] private GetterEnemyRotation _getterRotation;
+    [SerializeField] private GetterShooting _getterShooting;
 
     private void Awake()
     {
-        _gun.Initialize(_getterRotation.GetRotation());
+        _gun.Initialize(_getterRotation.GetRotation(),_getterShooting.GetAttack());
     }
 }
