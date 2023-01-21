8eef752cdf137b5f36b6f2ea6360c7537ae53d82
JustLink <linki_mail@mail.ru>
2023-01-21 13:16:18 +0400

    Player rotation by mouse

--- GJ Score Space 24/Assets/Scripts/Player/Player.cs
+++ GJ Score Space 24/Assets/Scripts/Player/Player.cs
@@ -1,18 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     private IMove _move;
+    private IRotation _rotation;
     private Health _health;
-    public void Initialize(IMove move, Health health)
+    
+    public void Initialize(IMove move, IRotation rotation, Health health)
     {
         _move = move;
+        _rotation = rotation;
         _health = health;
     }
+    
     private void FixedUpdate()
     {
         _move?.Move();
+        _rotation?.Rotate();
     }
 }
